53844d828719caa1c1623a36493704347ab91dd1
PG Dev <pgateway.dev@gmail.com>
2021-02-03 23:03:54 +0800

    [no-ticket][LW]updating gitignore and env path

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config({ path: __dirname + "/.env" });
 const moment = require('moment');
 const emailHelper = require('./emailHelper');
 const utils = require('./utils');
